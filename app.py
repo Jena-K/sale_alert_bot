ed3e6fe83d99af921370e16080f26b6305f08d14
Yonghun <lunaliku@gmail.com>
2023-12-18 02:50:29 +0900

    Local version ended

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
+# Import the config function from decouple
 import requests
-from bs4 import BeautifulSoup
-import time, subprocess, sqlite3
+import subprocess, sqlite3
 
 from flask import Flask, request
-from decouple import config  # Import the config function from decouple
+from decouple import config  
+
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import Updater, CommandHandler, CallbackQueryHandler
+
 
 # Load the bot token and chat ID from the .env file
 BOT_TOKEN = config('BOT_TOKEN')
-CHAT_ID = config('CHAT_ID')
 
 # List to store keywords
 keywords = []
@@ -15,9 +18,12 @@ keywords = []
 # Initialize Flask app
 app = Flask(__name__)
 
+# Create db at first run
+subprocess.run(['python', 'create_db.py'])
+
 # Run get items
 subprocess.Popen(['python', 'fm_keywords.py'])
-subprocess.run(['python', 'create_db.py'])
+
 
 # Define a route to handle incoming updates from Telegram
 @app.route('/webhook', methods=['POST'])
@@ -34,7 +40,6 @@ def webhook():
 
         # Handle /add_key command
         if text.startswith('/addkey '):
-            print(user_id, keyword)
             add_keyword(user_id, keyword)
 
         # Handle /del_key command
@@ -43,16 +48,11 @@ def webhook():
 
     return 'success', 200
 
-# # Function to add a keyword to the list
-# def add_key(text):
-#     keyword = text[len('/add_key '):].strip()
-#     if keyword:
-#         keywords.append(keyword)
-#         send_message("Keyword added successfully.")
+# Function to Insert a keywords into the table
 def add_keyword(user_id, keyword):
     if keyword:
         keywords.append(keyword)
-        send_message("Keyword added successfully.")
+        send_message(user_id, "Keyword added successfully.")
     
     conn = sqlite3.connect('keywords.db')
     cursor = conn.cursor()
@@ -63,11 +63,11 @@ def add_keyword(user_id, keyword):
     conn.commit()
     conn.close()
     
-    message = send_message("Keyword added successfully.")
+    message = send_message(user_id,  "Keyword added successfully.")
     
     send_message(user_id, message)
 
-# # Function to get a keywords from the list
+# Function to get a keywords from the table
 def get_keywords(user_id):
     conn = sqlite3.connect('keywords.db')
     cursor = conn.cursor()
@@ -80,18 +80,11 @@ def get_keywords(user_id):
     
     return keywords
 
-# # Function to delete a keyword from the list
-# def del_key(text):
-#     keyword = text[len('/del_key '):].strip()
-#     if keyword in keywords:
-#         keywords.remove(keyword)
-#         send_message("Keyword deleted successfully.")
-#     else:
-#         send_message("Keyword not found.")
+# Function to Delete a keywords to the table
 def delete_keyword(user_id, keyword):
     
     if keyword not in get_keywords(user_id):
-        send_message("Keyword not found.")
+        send_message(user_id, "Keyword not found.")
         return
 
     conn = sqlite3.connect('keywords.db')
@@ -103,7 +96,7 @@ def delete_keyword(user_id, keyword):
     conn.commit()
     conn.close()
     
-    send_message("Keyword deleted successfully.")
+    send_message(user_id, "Keyword deleted successfully.")
 
 
 # Function to send a message to the chat
@@ -115,66 +108,3 @@ def send_message(user_id, message):
 if __name__ == '__main__':
     app.run(port=5000)
     
-#-----------------------------------------
-
-
-#_---------------------------------------
-
-# from flask import Flask, request
-# import pprint
-# import requests
-# from decouple import config
-
-# app = Flask(__name__)       # app 초기화 과정
-
-# token = config('BOT_TOKEN')
-# api_url = f'https://api.telegram.org/bot{token}'
-
-# # http://127.0.0.1:5000/
-# @app.route('/')  # Decorator  # root 디렉토리로 들어오면 happy hacking를 출력시킬거란 의미
-# def root():
-#     return '<h1>Happy Hacking!</h1>' \
-#            '<h3>root 페이지입니다.</h3>'
-
-# @app.route(f'/{token}', methods=['POST'])
-# def telegram():    
-#     # [1] 방식
-#     response = request.get_json()
-#     # print(request.get_json())  # 사용자가 텔레그램 웹에서 메시지를 보내면 flask 서버에서 프린트 된다.  # {'update_id': 385295894, 'message': {'message_id': 27, 'from': {'id': 728488945, 'is_bot': False, 'first_name': 'River', 'last_name': 'Kang', 'username': 'kangpro404', 'language_code': 'en'}, 'chat': {'id': 728488945, 'first_name': 'River', 'last_name': 'Kang', 'username': 'kangpro404', 'type': 'private'}, 'date': 1559530336, 'text': '11시 52분'}}
-#     # 파일 보기가 좋지 않으니 파일 상단에 import pprint 해서 아래와 같이 사용하면 좋다.
-#     pprint.pprint(request.get_json())
-    
-#     # JSON에서 사용자의 id 추출
-#     chat_id = response['message']['from']['id']
-#     print('사용자 아이디(chat_id): ', chat_id)
-    
-#     # JSON에서 사용자가 입력한 text 추출
-#     chat_text = response['result']['message']['text']
-#     print('사용자가 보낸 문자(chat_text): ', chat_text)
-    
-#     # 사용자가 입력한 걸 챗봇이 똑같이 출력
-#     requests.get(f'{api_url}/sendMessage?chat_id={chat_id}&text={chat_text}')
-#     #[2] 방식 - 사용자가 메시지를 입력했는지 안했는지 체크하고 결과를 출력하는 로직
-#     pprint.pprint(request.get_json())
-#     message = request.get_json().get('message')
-#     if message is not None:
-#         chat_id = message.get('from').get('id')
-#         chat_text = message.get('text')
-#         requests.get(f'{api_url}/sendMessage?chat_id={chat_id}&text={chat_text}')
-
-#         # # 추가 예시
-#         if text == 'hi':
-#             # 점심메뉴를 확인하는 코드
-#             text = '점심메뉴'
-#         elif text == '로또':
-#             # 로또를 확인하는 코드
-#             text = '특정 로또번호'
-
-#     return 'hello', 200
-
-# # ※ 아래 함수?는 맨 아래에 둘 것!(위의 app.route 함수를 다 실행 한 뒤에 app.run 앱을 실행하겠다기 때문에)
-# # python name.py로 설정할 수 있도록 설정방법
-# # app.py 파일이 'python app.py'로 시작되었을 때 서버를 시작하겠다라는 의미.
-# if __name__ == '__main__':
-#     app.run(host='0.0.0.0', port='5000', debug=True)
-#     # '서버가 실행이 되어있는동안 수정이 되면 자동으로 재시작을 하겠다'라는 의미
--- fm_keywords.py
+++ fm_keywords.py
@@ -8,13 +8,45 @@ def load_keywords(file_path):
     with open(file_path, 'r', encoding='utf-8') as file:
         return [line.strip() for line in file]
 
-def send_telegram_message(bot_token, chat_id, message):
-    send_url = f'https://api.telegram.org/bot{bot_token}/sendMessage'
-    params = {'chat_id': chat_id, 'text': message}
-    requests.get(send_url, params=params)
+# alert_messages: [chat_ids, title, post_url]
+def send_telegram_message(alert_messages, bot_token):
+    
+    for alert_message in alert_messages:
+        chat_ids, title, post_url = alert_message
+        message = f"{title}\n{post_url}"
+        
+        for chat_id in chat_ids:
+            send_url = f'https://api.telegram.org/bot{bot_token}/sendMessage'
+            params = {'chat_id': chat_id, 'text': message}
+            requests.get(send_url, params=params)
 
-def fetch_posts(url, fetched_posts, keywords, bot_token, user_id):
+def fetch_chat_ids(posts_to_fetch):
+    alert_messages = []
+
+    # Connect to database
+    conn = sqlite3.connect('keywords.db')
+    cursor = conn.cursor()
+    
+    for title, post_url in posts_to_fetch:
+        chat_ids = set()
+        
+        # Fetch keywords for the user associated with this post
+        cursor.execute('SELECT user_id, keyword FROM keywords')
+        user_keywords = [row for row in cursor.fetchall()]
+        
+        for chat_id, keyword in user_keywords:
+            if keyword.lower() in title.lower():
+                chat_ids.add(chat_id)
+        
+        alert_messages.append([chat_ids, title, post_url])
+        
+    conn.close()
+    
+    return alert_messages
+
+def fetch_posts(url, fetched_posts):
     try:
+        posts_to_fetch = []
         response = requests.get(url)
         soup = BeautifulSoup(response.content, 'html.parser')
 
@@ -22,8 +54,6 @@ def fetch_posts(url, fetched_posts, keywords, bot_token, user_id):
 
         for post in posts:
             title_element = post.select_one('div.li h3.title a')
-            if not title_element:
-                continue
 
             title = title_element.get_text(strip=True)
             post_url = 'https://www.fmkorea.com' + title_element['href']
@@ -32,44 +62,24 @@ def fetch_posts(url, fetched_posts, keywords, bot_token, user_id):
                 continue
 
             fetched_posts.add(post_url)
+            
+            posts_to_fetch.append((title, post_url))
 
-            # Fetch keywords for the user associated with this post
-            conn = sqlite3.connect('keywords.db')
-            cursor = conn.cursor()
-            cursor.execute('SELECT keyword FROM keywords WHERE user_id = ?', (user_id,))
-            user_keywords = [row[0] for row in cursor.fetchall()]
-            conn.close()
-
-            if any(keyword.lower() in title.lower() for keyword in user_keywords):
-                message = f"{title}\n{post_url}"
-                send_telegram_message(bot_token, user_id, message)
-                
-            # Check if the title contains any of the keywords
-            # if any(keyword.lower() in title.lower() for keyword in keywords):
-            #     message = f"{title}\n{post_url}"
-            #     send_telegram_message(bot_token, chat_id, message)
-
+        return posts_to_fetch
+    
     except Exception as e:
         print(f"Error occurred: {e}")
 
 def main():
     url = 'https://www.fmkorea.com/hotdeal'
     bot_token = config('BOT_TOKEN')  # Load bot_token from .env file
-    chat_id = config('CHAT_ID')  # Load chat_id from .env file
     fetched_posts = set()
-    # keywords = load_keywords('keywords.txt')  # Load keywords from file
-
-    conn = sqlite3.connect('keywords.db')
-    cursor = conn.cursor()
-
-    # Select all keywords for the specified user
-    cursor.execute('SELECT DISTINCT keyword FROM keywords')
-    
-    keywords = [row for row in cursor.fetchall()]
-    pprint(keywords)
 
     while True:
-        fetch_posts(url, fetched_posts, keywords, bot_token, chat_id)
+        posts_to_fetch = fetch_posts(url, fetched_posts)
+        alert_messages = fetch_chat_ids(posts_to_fetch)
+        send_telegram_message(alert_messages, bot_token)
+        
         time.sleep(10)  # Check every 10 seconds
     
 if __name__ == "__main__":
